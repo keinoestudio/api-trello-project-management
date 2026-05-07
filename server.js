965728bbc10ced8ce6562b999e9479a66cfc6a00
keinoestudio <oficina@keinoestudio.com>
2026-05-07 14:51:35 +0200

    refactor: adds .env const for security enhancing

--- server.js
+++ server.js
@@ -8,11 +8,16 @@ dotenv.config()
 //Init server at "server" namespace
 const server = express()
 
+//Brings data from .env
+
+const port = process.env.PORT
+const host = process.env.HOST
+
 server.get('/', (req, res) => {
     res.send("Server is running")
 })
 
-server.listen(3000, () => {
-    console.log(`Server is running at http://localhost:3000`)
+server.listen(port, host, () => {
+    console.log(`Server is running at http://${host}:${port}`)
 })
 
