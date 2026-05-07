04bb1a03c584438d89b29c3a2e8b21c8562e5c87
keinoestudio <oficina@keinoestudio.com>
2026-05-07 17:49:22 +0200

    feat: adds ngrok config

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 import express from "express"
 import dotenv from "dotenv"
 
+//setup ngrok
+
+import ngrok from "@ngrok/ngrok"
+
 
 //Adds dotenv support
 dotenv.config()
@@ -9,16 +13,23 @@ dotenv.config()
 //Init server at "server" namespace
 const server = express()
 
+//Middleware for json parsing
+server.use(express.json())
+
 //Brings data from .env
 
 const port = process.env.PORT
 const host = process.env.HOST
 
 server.get('/', (req, res) => {
-    res.send("Server is running")
+    res.send("Trello server is running and active")
 })
 
 server.listen(port, host, () => {
     console.log(`Server is running at http://${host}:${port}`)
 })
 
+
+//expose app with ngrok
+ngrok.connect({ addr: port, authtoken_from_env: true })
+	.then(listener => console.log(`Ingress established at: ${listener.url()}`));
