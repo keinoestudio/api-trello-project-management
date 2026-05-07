6f8aefccf9436ec07ec6892511f27bc9848354cf
keinoestudio <oficina@keinoestudio.com>
2026-05-07 14:48:22 +0200

    feat: adds simple server and test

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
+//Libraries import
 import express from "express"
 import dotenv from "dotenv"
 
+//Adds dotenv support
 dotenv.config()
+
+//Init server at "server" namespace
+const server = express()
+
+server.get('/', (req, res) => {
+    res.send("Server is running")
+})
+
+server.listen(3000, () => {
+    console.log(`Server is running at http://localhost:3000`)
+})
+
